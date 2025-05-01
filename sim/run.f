a74df823f7dc4cb7d0b2c2f100089f1369748639
dschrag2 <dschrag2@nd.edu>
2025-05-01 14:21:02 -0400

    Getting things running for tb2

--- sim/run.f
+++ sim/run.f
@@ -1,10 +1,14 @@
 # Source files
 ../src/riscv_stub.sv
-../tb/memories.sv
-../tb/riscv_stub_tb.sv
+#../tb/memories.sv
+#../tb/riscv_stub_tb.sv
+../tb/riscv_stub_tb2.sv
+../tb/data_mem.sv
+../tb/instr_mem.sv
 
 # Top-level module
--top riscv_stub_tb
+#-top riscv_stub_tb
+-top riscv_stub_tb2
 
 # Simulation options
 # -access +rwc
