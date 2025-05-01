f5698fdd68ff89b07f8834471afa81e6c38d6c10
Max Johnson <mjohns79@student06.cse.nd.edu>
2025-05-01 17:34:32 -0400

    commiting my run.f updates

--- sim/run.f
+++ sim/run.f
@@ -1,12 +1,12 @@
 # Source files
 ../src/riscv_stub.sv
 ../tb/memories.sv
-../tb/riscv_stub_tb.sv
-#../tb/riscv_stub_tb2.sv
+#../tb/riscv_stub_tb.sv
+../tb/riscv_stub_tb2.sv
 
 # Top-level module
--top riscv_stub_tb
-#-top riscv_stub_tb2
+#-top riscv_stub_tb
+-top riscv_stub_tb2
 
 # Simulation options
 # -access +rwc
